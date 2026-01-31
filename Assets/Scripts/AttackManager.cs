b4f22b1c1fa064ceebce56d63ac04e3a98a444d6
Gryphon Picciottoli <200626774@student.georgianc.on.ca>
2026-01-30 21:12:47 -0500

    AttackManager and fixing the postprocessing issue

---
+++ Assets/Scripts/AttackManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+
+    public bool is_attacking = false;
+
+
+    public Coroutine IAttack()
+    {
+        return null;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
